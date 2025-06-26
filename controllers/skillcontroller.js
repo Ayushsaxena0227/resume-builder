2fc88c74bd867f76b385a2c323084ecf5409c269
Ayushsaxena0227 <saxenaayush381@gmail.com>
2025-06-26 14:51:18 +0530

    Added skills api(add, get, delete) and tested

---
+++ controllers/skillcontroller.js
@@ -0,0 +1,71 @@
+const { db } = require("../firebase/config");
+
+//  Add a new skill
+exports.addSkill = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { name } = req.body;
+
+    if (!name)
+      return res.status(400).json({ message: "Skill name is required." });
+
+    const skillRef = await db
+      .collection("users")
+      .doc(userId)
+      .collection("resume")
+      .doc("skills");
+    const skillsCollection = skillRef.collection("items");
+
+    const newSkill = await skillsCollection.add({ name });
+
+    res
+      .status(200)
+      .json({ message: "Skill added successfully!", id: newSkill.id });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//  Get all skills
+exports.getSkills = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const skillsSnap = await db
+      .collection("users")
+      .doc(userId)
+      .collection("resume")
+      .doc("skills")
+      .collection("items")
+      .get();
+
+    const skills = skillsSnap.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    res.status(200).json(skills);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//  Delete a skill by skillId
+exports.deleteSkill = async (req, res) => {
+  try {
+    const { userId, skillId } = req.params;
+
+    await db
+      .collection("users")
+      .doc(userId)
+      .collection("resume")
+      .doc("skills")
+      .collection("items")
+      .doc(skillId)
+      .delete();
+
+    res.status(200).json({ message: "Skill deleted successfully!" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const cors = require("cors");
 const app = express();
 
 const userRoutes = require("./routes/userRoutes");
+const skillroutes = require("./routes/skillRoutes");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
+app.use("/api/user", skillroutes);
 
 app.get("/", (req, res) => {
   res.send("Resume Builder API is running...");
